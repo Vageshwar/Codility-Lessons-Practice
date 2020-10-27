01cac9645b0c9edb8b045062f3b4bf72d63742c6
Vageshwar Yadav <vageshwaryadav@gmail.com>
2020-10-27 16:34:27 +0530

    First

---
+++ tapeEqui.py
@@ -0,0 +1,39 @@
+# you can write to stdout for debugging purposes, e.g.
+# print("this is a debug message")
+import math
+
+def solution(A):
+    # write your code in Python 3.6
+    n = len(A)
+    l = 0
+    s = sum(A)
+    ans = math.inf
+    for i in A[:-1]:
+        s -= i
+        l += i
+        ans = min(ans,abs(s-l))
+    return ans
+
+
+# A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+# Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+# The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+# In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+# For example, consider array A such that:
+
+#   A[0] = 3
+#   A[1] = 1
+#   A[2] = 2
+#   A[3] = 4
+#   A[4] = 3
+# We can split this tape in four places:
+
+# P = 1, difference = |3 − 10| = 7
+# P = 2, difference = |4 − 9| = 5
+# P = 3, difference = |6 − 7| = 1
+# P = 4, difference = |10 − 3| = 7
+# Write a function:
